02a89e0bb3d0e4ede77907e2b0c4ea36243c4d74
mms612 <fhf612@naver.com>
2024-09-12 16:28:38 +0900

    Create day02-1.c

---
+++ day2/day02-1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main() {
+	int op;
+	double num1, num2;
+	double result = 0;
+
+	printf("원하는 기능을 입력하시오.\n");
+	printf("1.더하기 2.빼기 3.곱하기 4.나누기\n");
+	printf("기능 : ");
+
+	scanf_s("%d", &op);
+
+	printf("첫 번째 수 : ");
+	scanf_s("%d", &num1);
+	printf("두 번째 수 : ");
+	scanf_s("%d", &num2);
+
+	if (op == 1) {
+		result = num1 + num2;
+	}
+	else if(op == 2) {
+		result = num1 - num2;
+	}
+	else if (op == 3){
+		result = num1 * num2;
+	}
+	else {
+		result = num1 / num2;
+	}
+
+	printf(" 값 : %d", result);
+
+	return 0;
+}
