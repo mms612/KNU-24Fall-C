da95279ddfee84f3b1f6908760403f8dee67d091
mms612 <fhf612@naver.com>
2024-09-23 14:44:28 +0900

    Create day04-2.c

---
+++ day04/day04-2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void swap(int* pa, int* pb);
+
+int main(void) {
+	
+	int a = 10;
+	int b = 20;
+
+	printf("a = %d, b = %d\n", a, b);
+
+	swap(&a, &b);
+
+	printf("a = %d, b = %d\n", a, b);
+
+	return 0;
+}
+
+void swap(int* pa, int* pb) {
+	int temp = *pa;
+	*pa = *pb;
+	*pb = temp;
+}
